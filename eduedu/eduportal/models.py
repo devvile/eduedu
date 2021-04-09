5fc5b800b18e542e7b30bb331685362f21610a17
Dottore <patryk.czemierowski@gmail.com>
2021-04-10 01:19:22 +0200

    [DB] Note, Question, Question_Category, Note_Category + migration@

--- eduedu/eduportal/models.py
+++ eduedu/eduportal/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 
-class Wpis(models.Model):
+class Note(models.Model):
     head = models.CharField(max_length=255)
     category = models.CharField(max_length=255)
     body = models.TextField(max_length=2000)
+
+class Question(models.Model):
+    question = models.CharField(max_length=255)
+    category = models.CharField(max_length=255)
+    answer = models.TextField(max_length=2000)
+
+class Question_Category(models.Model):
+    name = models.CharField(max_length=255)
+
+class Note_Category(models.Model):
+    name = models.CharField(max_length=255)
